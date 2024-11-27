9a42d023587126db9c0b8b4fcbc2e9fc06db4e40
Seksan Thichon <seksan1467@gmail.com>
2024-11-27 14:52:32 +0700

    Update Lab2Extra.java

--- Lab2/Lab2Extra.java
+++ Lab2/Lab2Extra.java
@@ -5,8 +5,14 @@ public class Lab2Extra {
         int max=0;
         Scanner sc = new Scanner(System.in);
         System.out.print("input: ");
-        String data = sc.next();
+        String data = sc.nextLine();
+        String[] arr = data.split(",");
+        int[] nums = new int[arr.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(arr[i]);
+        }
+        Arrays.sort(nums);
        
-        System.out.println("Output: " + max);
+        System.out.println("Output: " + nums[nums.length-1]);
     }
 }
